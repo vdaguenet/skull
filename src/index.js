e4bee2326075e0d93ee9ab0ed95beff73c74cbbd
Valentin Daguenet <valentin.daguenet@gmail.com>
2014-10-24 11:35:53 +0200

    integrate sound in main

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-var SceneManager = require('./lib/SceneManager.js');
-var AsteroidScene = require('./lib/AsteroidScene.js');
+var SceneManager = require('./lib/SceneManager.js'),
+    AsteroidScene = require('./lib/AsteroidScene.js'),
+    Audio = require('./lib//Audio.js');
+
+var audioAnalyzer = new Audio('../assets/sound/Biome - Shaman.mp3');
 
 (function() {
     var sm = new SceneManager(document.getElementById('render'));
     var s = new AsteroidScene();
     sm.register(s);
     sm.play(0);
+    audioAnalyzer.load(function() {
+        audioAnalyzer.play();
+        requestAnimationFrame(analyze);
+    });
 })();
+
+function analyze() {
+    requestAnimationFrame(analyze);
+    var stream = audioAnalyzer.getFrequencyAnalysis();
+    console.log(stream);
+}
